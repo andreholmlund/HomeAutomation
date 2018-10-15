e3c3f256b88627134ab7094ed22202855034b601
André Holmlund <andre.holmlund@gmail.com>
2018-10-15 21:44:11 +0200

    Add Xamarin app

---
+++ HomeAutomation.App/HomeAutomation.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HomeAutomation.App.Models;
+
+namespace HomeAutomation.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ HomeAutomation.App/HomeAutomation.App/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeAutomation.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
